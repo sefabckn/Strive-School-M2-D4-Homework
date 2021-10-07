095a667aeed3daa792f35d1694ba711a7e8ff228
sefabckn <sefabckn@gmail.com>
2021-10-07 15:16:57 +0200

    files created. Html page ,buttons and input areas created

---
+++ app.js
@@ -0,0 +1,21 @@
+// Take the username input as string
+// add name button for each input we must click on add button and will show the name under the page(like a todo list)
+//another input area for team names and another button to creat teams
+// assignment button should add the names to the teams area
+//but teams section should be appear when we click on the assign button
+//for each click on assign button we must copy each name to any team
+
+
+const takingInputs = function() {
+    let takeInput = document.getElementsByClassName("input-group-text")
+    return takeInput
+}
+const getClickEvent = function() {
+
+
+}
+window.onload = function() {
+
+    takingInputs()
+    getClickEvent()
+}
